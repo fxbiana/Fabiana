34cb1aec4e4f23a7b4af9a8fdb3c612e18721d87
Fabiana <58375132+fxbiana@users.noreply.github.com>
2024-02-07 21:45:37 -0800

    Update testing.js

--- m05/testing.js
+++ m05/testing.js
@@ -1,13 +1,9 @@
 var i = prompt("Please a number 1-10", "0");
 var msg = '<h2>Multiplication Table</h2>';
-if (person != null) {
-  document.getElementById("demo").innerHTML =
-  "Hello " + person + "! How are you today?";
    while (i < 11) {
     msg += i + ' * ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
-}
 var el = document.getElementById('blackboard');
 el.innerHTML = msg;
 
